b8f4fc72a7fd54c60245779a18c53f15d9979fe0
Sidhant Kumar Nayak <sidhantn99@gmail.com>
2023-12-30 12:17:31 +0530

    Update first exercise

---
+++ src/Message.tsx
@@ -0,0 +1,13 @@
+
+// PascalCasing
+function Message() {
+    const name="Sidhant";
+    if(name) {
+        return <h1>Hello, {name}!</h1>;
+    }
+    else {
+        return <h1>Hello, Stranger!</h1>;
+    }
+}
+
+export default Message;
