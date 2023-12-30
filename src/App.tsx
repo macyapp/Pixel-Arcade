136e94f08b30a346edae9d6639a61aae9baeebc9
Sidhant Kumar Nayak <sidhantn99@gmail.com>
2023-12-30 13:43:14 +0530

    Add item selection functionality to ListGroup component

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,12 @@ import ListGroup from "./components/ListGroup";
 function App() {
   const items = ["New York", "London", "Paris", "Berlin", "Tokyo"];
 
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
   return (
     <div>
-      <ListGroup items={items} heading="Cities"/>
+      <ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem} />
     </div>
   );
 }
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -4,9 +4,11 @@ import { useState } from "react";
 interface Props {
   items: string[];
   heading: string;
+  // (item: string) => void;
+  onSelectItem: (item: string) => void;
 }
 
-function ListGroup({ items, heading }: Props) {
+function ListGroup({ items, heading, onSelectItem }: Props) {
   // Hook
   const [selectedIndex, setSelectedIndex] = useState(-1);
 
@@ -23,7 +25,10 @@ function ListGroup({ items, heading }: Props) {
                 : "list-group-item"
             }
             key={item}
-            onClick={() => setSelectedIndex(index)}
+            onClick={() => {
+              setSelectedIndex(index);
+              onSelectItem(item);
+            }}
           >
             {item}
           </li>
