16a17ddf049592ddf8b9a541013e8a26482848d6
Sidhant Kumar Nayak <sidhantn99@gmail.com>
2023-12-30 15:50:31 +0530

    Add alert functionality to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
+import { useState } from "react";
 import Alert from "./components/Alert";
+import Button from "./components/Button";
 
 function App() {
+  const [alertVisible, setAlertVisibility] = useState(false);
   return (
     <div>
-      <Alert>
-        Hello, <span>World!</span>
-      </Alert>
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisibility(false)}>My alert</Alert>
+      )}
+      <Button onClickButton={() => setAlertVisibility(true)}>Show Alert</Button>
     </div>
   );
 }
