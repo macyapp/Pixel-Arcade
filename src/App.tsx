a0e4e6d4118848f75fe78d79c7a64d5143335c1c
Sidhant Kumar Nayak <sidhantn99@gmail.com>
2023-12-31 13:08:33 +0530

    Refactor handleClick function to use immer for immutability. Simplification of state updation.

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@
 // import Message from "./components/Message";
 import Button from "./components/Button";
 import { useEffect, useState, useRef } from "react";
+import { produce } from "immer";
 
 const App = () => {
   const [bugs, setBugs] = useState([
@@ -13,7 +14,16 @@ const App = () => {
   const isInitialRender = useRef(true);
 
   const handleClick = () => {
-    setBugs(bugs.map((bug) => (bug.id === 1 ? { ...bug, fixed: true } : bug)));
+    // setBugs(bugs.map((bug) => (bug.id === 1 ? { ...bug, fixed: true } : bug)));
+    // draft is a proxy object that records the changes we're going to apply to the bugs array
+    setBugs(
+      produce((draft) => {
+        const bug = draft.find((bug) => bug.id === 1);
+        if (bug) {
+          bug.fixed = true;
+        }
+      })
+    );
   };
 
   // Use useEffect to log changes to the bugs array
@@ -21,10 +31,9 @@ const App = () => {
     if (isInitialRender.current) {
       // If it's the first render, update the ref and don't log
       isInitialRender.current = false;
-    }
-    else {
+    } else {
       // Not the initial render, so log the updated bugs
-      console.log('Updated bugs:', bugs);
+      console.log("Updated bugs:", bugs);
     }
   }, [bugs]); // This effect runs whenever the bugs array changes
 
@@ -33,6 +42,11 @@ const App = () => {
       <Button color="primary" onButtonClick={handleClick}>
         Click Me
       </Button>
+      {bugs.map((bug) => (
+        <p key={bug.id}>
+          {bug.title} {bug.fixed ? "Fixed" : "Not Fixed"}
+        </p>
+      ))}
     </>
   );
 };
