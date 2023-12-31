4161909b494a3f59eb573b40981ea6eba45ce446
Sidhant Kumar Nayak <sidhantn99@gmail.com>
2023-12-31 12:51:19 +0530

    Updating Array of Objects. Only one object without creating a whole new array.

--- src/App.tsx
+++ src/App.tsx
@@ -9,13 +9,23 @@ const App = () => {
     { id: 2, title: "Bug 2", fixed: false },
   ]);
 
+  // Create a ref to track the initial render
+  const isInitialRender = useRef(true);
+
   const handleClick = () => {
     setBugs(bugs.map((bug) => (bug.id === 1 ? { ...bug, fixed: true } : bug)));
   };
 
   // Use useEffect to log changes to the bugs array
   useEffect(() => {
-    console.log('Updated bugs:', bugs);
+    if (isInitialRender.current) {
+      // If it's the first render, update the ref and don't log
+      isInitialRender.current = false;
+    }
+    else {
+      // Not the initial render, so log the updated bugs
+      console.log('Updated bugs:', bugs);
+    }
   }, [bugs]); // This effect runs whenever the bugs array changes
 
   return (
