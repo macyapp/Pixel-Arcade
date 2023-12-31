474833605d9ee54bf0afa01c0b0e68d6652079b7
Sidhant Kumar Nayak <sidhantn99@gmail.com>
2023-12-31 13:29:01 +0530

    Refactor game state and update player name

--- src/App.tsx
+++ src/App.tsx
@@ -5,25 +5,18 @@ import { useEffect, useState, useRef } from "react";
 import { produce } from "immer";
 
 const App = () => {
-  const [bugs, setBugs] = useState([
-    { id: 1, title: "Bug 1", fixed: false },
-    { id: 2, title: "Bug 2", fixed: false },
-  ]);
+  const [game, setGame] = useState({
+    id: 1,
+    player: {
+      firstName: "John",
+    },
+  });
 
   // Create a ref to track the initial render
   const isInitialRender = useRef(true);
 
   const handleClick = () => {
-    // setBugs(bugs.map((bug) => (bug.id === 1 ? { ...bug, fixed: true } : bug)));
-    // draft is a proxy object that records the changes we're going to apply to the bugs array
-    setBugs(
-      produce((draft) => {
-        const bug = draft.find((bug) => bug.id === 1);
-        if (bug) {
-          bug.fixed = true;
-        }
-      })
-    );
+    setGame({ ...game, player: { ...game.player, firstName: "Sid" } });
   };
 
   // Use useEffect to log changes to the bugs array
@@ -31,22 +24,19 @@ const App = () => {
     if (isInitialRender.current) {
       // If it's the first render, update the ref and don't log
       isInitialRender.current = false;
-    } else {
-      // Not the initial render, so log the updated bugs
-      console.log("Updated bugs:", bugs);
     }
-  }, [bugs]); // This effect runs whenever the bugs array changes
+    else {
+      // Not the initial render, so log the updated bugs
+      console.log(game);
+    }
+  }, [game]); // This effect runs whenever the bugs array changes
 
   return (
     <>
       <Button color="primary" onButtonClick={handleClick}>
         Click Me
       </Button>
-      {bugs.map((bug) => (
-        <p key={bug.id}>
-          {bug.title} {bug.fixed ? "Fixed" : "Not Fixed"}
-        </p>
-      ))}
+      <p>Player Name: {game.player.firstName}</p>
     </>
   );
 };
