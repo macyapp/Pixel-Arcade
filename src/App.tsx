c774e412f63fcf9f79eb052ac55025cd89af77fc
Sidhant Kumar Nayak <sidhantn99@gmail.com>
2023-12-31 17:28:07 +0530

    Sharing State between components

--- src/App.tsx
+++ src/App.tsx
@@ -3,44 +3,18 @@
 import Button from "./components/Button";
 import { useEffect, useState, useRef } from "react";
 import { produce } from "immer";
+import NavBar from "./components/NavBar";
+import Cart from "./components/Cart";
 
 const App = () => {
-  const [game, setGame] = useState({
-    id: 1,
-    player: {
-      firstName: "John",
-    },
-  });
-
-  // Create a ref to track the initial render
-  const isInitialRender = useRef(true);
-
-  const handleClick = () => {
-    // setGame({ ...game, player: { ...game.player, firstName: "Sid" } });
-    setGame(
-      produce((draft) => {
-        draft.player.firstName = "Sid";
-      })
-    );
-  };
-
-  // Use useEffect to log changes to the bugs array
-  useEffect(() => {
-    if (isInitialRender.current) {
-      // If it's the first render, update the ref and don't log
-      isInitialRender.current = false;
-    } else {
-      // Not the initial render, so log the updated bugs
-      console.log(game);
-    }
-  }, [game]); // This effect runs whenever the bugs array changes
+  const [cartItems, setCartItems] = useState(["Product1", "Product2"]);
 
   return (
     <>
-      <Button color="primary" onButtonClick={handleClick}>
-        Click Me
-      </Button>
-      <p>Player Name: {game.player.firstName}</p>
+      <div>
+        <NavBar cartItemsCount={cartItems.length} />
+        <Cart cartItems={cartItems} onClear={() => setCartItems([])} />
+      </div>
     </>
   );
 };
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,12 @@
+
+interface NavBarProps {
+    cartItemsCount: number
+}
+
+const NavBar = ({cartItemsCount}:NavBarProps) => {
+  return (
+    <div>NavBar: {cartItemsCount}</div>
+  )
+}
+
+export default NavBar
