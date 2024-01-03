fb4b6f219fc53f160f81233feb556c27d82800e2
Sidhant Kumar Nayak <sidhantn99@gmail.com>
2024-01-03 18:29:11 +0530

    Expense Tracker

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,48 @@
 // import { useEffect, useState, useRef } from "react";
 // import { produce } from "immer";
 // import ExpandableText from "./components/ExpandableText";
-import Form from "./components/Form";
+// import Form from "./components/Form";
+import { useState } from "react";
+import ExpenseList from "./expense-tracker/components/ExpenseList";
+import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
+import ExpenseForm from "./expense-tracker/components/ExpenseForm";
+import categories from "./expense-tracker/categories";
 
-const App = () => {
+function App() {
+  const [selectedCategory, setSelectedCategory] = useState("");
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: "Pizza", amount: 100, category: "Entertainment" },
+    { id: 2, description: "Rent", amount: 10000, category: "Utilities" },
+    { id: 3, description: "Salary", amount: 100000, category: "Income" },
+    { id: 4, description: "Milk", amount: 5000, category: "Groceries" },
+  ]);
+
+  const visibleExpenses = selectedCategory
+    ? expenses.filter((e) => e.category === selectedCategory)
+    : expenses;
+
   return (
     <>
-      <Form/>
+      <div className="mb-5">
+        <ExpenseForm
+          onSubmit={(expense) =>
+            setExpenses([...expenses, { ...expense, id: expenses.length + 1 }])
+          }
+        />
+      </div>
+      <div className="mb-3">
+        <ExpenseFilter
+          onSelectedCategory={(category) => setSelectedCategory(category)}
+        />
+      </div>
+      <ExpenseList
+        expenses={visibleExpenses}
+        onDeleteExpense={(id) =>
+          setExpenses(expenses.filter((e) => e.id !== id))
+        }
+      />
     </>
   );
-};
+}
 
 export default App;
---
+++ src/expense-tracker/categories.ts
@@ -0,0 +1,3 @@
+const categories = ["Groceries", "Utilities", "Entertainment", "Income"] as const;
+
+export default categories;
